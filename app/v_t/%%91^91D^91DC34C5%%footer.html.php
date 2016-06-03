94c1d0114b390efff241eb572676a01a474ed12c
Liu <liuxiaolong02@internal.baidu.com>
2016-06-03 15:49:23 +0800

    初始化

---
+++ app/v_t/%%91^91D^91DC34C5%%footer.html.php
@@ -0,0 +1,6 @@
+<?php /* Smarty version 2.6.26, created on 2016-06-03 09:32:55
+         compiled from simpla/common/footer.html */ ?>
+</div>
+<script>$(document).ready(function(){if($("#goods_sn").length)$("#goods_sn").focus();})</script>
+</body>
+</html>
